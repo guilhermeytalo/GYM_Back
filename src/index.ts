5fdaef0ad97c21aa4594cdd30e10173b2c4b4b65
Guilherme Ytalo <gydaytalo@gmail.com>
2023-04-22 23:52:07 -0300

    added models

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { Pool } from "pg"
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -22,6 +23,11 @@ const connectToDB = async () => {
 }
 
 connectToDB();
+
+//midldleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.send("hi");
---
+++ src/models/index.ts
@@ -0,0 +1,22 @@
+import { DataTypes, Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(`postgres://postgres:123456@localhost:5432/discover`, {dialect: "postgres"});
+
+sequelize.authenticate().then(() => {
+    console.log(`Database connected to discovery`);
+}).catch((err) => {
+    console.log(err);
+});
+
+const db = {
+    Sequelize, 
+    sequelize,
+    users: require("./userModel")(sequelize, DataTypes),
+}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("./userModel")(sequelize, DataTypes);
+
+module.exports = db;
