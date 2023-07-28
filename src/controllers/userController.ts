048d906ae4da4d39620039e3a8d199eb1f1d6c24
Guilherme Ytalo <gydaytalo@gmail.com>
2023-07-28 16:43:38 -0300

    fix: expiresIn

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -22,10 +22,10 @@ export const signup = async (req: Request, res: Response) => {
 
     if (user) {
       let token = jwt.sign({id: user.id}, process.env.SECRET_KEY!, {
-        expiresIn: 21 * 60 * 60 * 1000,
+        expiresIn: 2592000,
       });
 
-      res.cookie('jwt', token, {maxAge: 24 * 60 * 60, httpOnly: true});
+      res.cookie('jwt', token, {maxAge: 2592000, httpOnly: true});
       console.log('user', JSON.stringify(user, null, 2));
       console.log(token);
 
