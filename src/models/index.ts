9767b5f8ece1becc3f38d4c71b504404a14bd8f1
Guilherme Ytalo <gydaytalo@gmail.com>
2023-07-28 17:04:01 -0300

    remove token console.log

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,22 +1,23 @@
 import { DataTypes, Sequelize } from 'sequelize';
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
 export const sequelize = new Sequelize({
-    host:  process.env.DB_HOST,
-    username: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT as string),
-    dialect: 'postgres',
+  host: DB_HOST,
+  username: DB_USER,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: parseInt(DB_PORT as string),
+  dialect: 'postgres',
 });
 
-
 export const db = {
-    Sequelize, 
-    sequelize,
-    users: require("./userModel")(sequelize, DataTypes),
-}
+  Sequelize,
+  sequelize,
+  users: require('./userModel')(sequelize, DataTypes),
+};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.users = require('./userModel') (sequelize, DataTypes);
+db.users = require('./userModel')(sequelize, DataTypes);
