c5a9d6f33d48f1d8d84693bd7b2b3e33c68509ab
Guilherme Ytalo <gydaytalo@gmail.com>
2023-09-20 07:11:16 -0300

    fix: local .env and controller order

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcryptjs';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { User } from '../utils/database';
 
@@ -12,8 +12,7 @@ export type IUser = {
 
 const tokenList: any = {};
 
-// Create a new user
-
+// Signing a user up
 export const signup = async (req: Request, res: Response) => {
   try {
     const { userName, email, password } = req.body;
@@ -28,11 +27,11 @@ export const signup = async (req: Request, res: Response) => {
     });
 
     if (user) {
-      let token = jwt.sign({id: user.id}, process.env.SECRET_KEY!, {
-        expiresIn: '30d',
+      let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY!, {
+        expiresIn: 1 * 21 * 60 * 60 * 1000,
       });
 
-      res.cookie('jwt', token, {maxAge: 1 * 24 * 60 * 60, httpOnly: true});
+      res.cookie('jwt', token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
       console.log('user', JSON.stringify(user, null, 2));
       console.log(token);
 
@@ -48,7 +47,7 @@ export const signup = async (req: Request, res: Response) => {
 // Login authentication
 export const login = async (req: Request, res: Response) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     const user = await User.findFirst({
       where: {
@@ -60,30 +59,80 @@ export const login = async (req: Request, res: Response) => {
       const isSame = await bcrypt.compare(password, user.password);
 
       if (isSame) {
-        let token = jwt.sign({id: user.id}, process.env.SECRET_KEY!, {
-          expiresIn: '30d',
+        let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY!, {
+          expiresIn: 1 * 24 * 60 * 60 * 1000,
         });
 
-        res.cookie('jwt', token, {maxAge: 1 * 24 * 60 * 60, httpOnly: true});
-        // console.log('user', JSON.stringify(user, null, 2));
-        // console.log(token);
+        res.cookie('jwt', token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+        console.log('user', JSON.stringify(user, null, 2));
+        console.log(token);
 
-        let refreshToken = jwt.sign({id: user.id}, process.env.SECRET_KEY!, {expiresIn: '30d'});
-
-        const response = {
-          "token": token,
-          "refreshToken": refreshToken,
-          "user": user,
-        }
-
-        tokenList[refreshToken] = response;
-        
-        return res.status(200).json(response);
+        return res.status(200).json(user);
       } else {
         return res.status(401).json({ message: 'Authentication failed' });
       }
     } else {
       return res.status(401).json({ message: 'Authentication failed' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Refresh  token
+
+export const refreshToken = async (req: Request, res: Response) => {
+  try {
+    const postData = req.body;
+    console.log('postData', postData);
+
+    if (postData.refreshToken && postData.refreshToken in tokenList) {
+      const user = {
+        id: postData.id,
+        email: postData.email,
+        userName: postData.userName,
+      };
+
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY!, {
+        expiresIn: '30d',
+      });
+
+      const response = {
+        token: token,
+      };
+
+      tokenList[postData.refreshToken].token = token;
+      res.status(200).json(response);
+    } else {
+      res.status(404).send('Invalid request');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete User
+
+export const deleteUser = async (req: Request, res: Response) => {
+  // delete user by email
+  try {
+    const { email } = req.body;
+    const user = await User.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user) {
+      await User.delete({
+        where: {
+          email: email,
+        },
+      });
+
+      return res.status(200).send('User deleted');
+    } else {
+      return res.status(404).send('User not found');
     }
   } catch (error) {
     console.log(error);
@@ -105,65 +154,6 @@ export const showAllUsers = async (req: Request, res: Response) => {
       }
     );
     return res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Refresh  token
-
-export const refreshToken = async (req: Request, res: Response) => {
-  try {
-    const postData = req.body;
-    console.log('postData', postData);
-
-    if ((postData.refreshToken) && (postData.refreshToken in tokenList)) {
-      const user = {
-        "id": postData.id,
-        "email": postData.email,
-        "userName": postData.userName,
-      }
-
-      const token = jwt.sign({id: user.id}, process.env.SECRET_KEY!, {expiresIn: '30d'});
-
-      const response = {
-        "token": token,
-      }
-
-      tokenList[postData.refreshToken].token = token;
-      res.status(200).json(response);
-
-    } else {
-      res.status(404).send('Invalid request');
-    }
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-// Delete User
-
-export const deleteUser = async (req: Request, res: Response) => {
-  // delete user by email
-  try {
-    const {email} = req.body;
-    const user = await User.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (user) {
-      await User.delete({
-        where: {
-          email: email,
-        },
-      });
-
-      return res.status(200).send('User deleted');
-    } else {
-      return res.status(404).send('User not found');
-    }
   } catch (error) {
     console.log(error);
   }
